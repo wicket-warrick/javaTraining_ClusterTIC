90458d379101963b89459d201d279dd2ed5b016a
Hugo Suárez <hugolsg@ext.inditex.com>
2024-02-10 14:16:32 +0100

    init training

---
+++ test01/src/main/java/com/clustertic/test01/Test01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.clustertic.test01;
+
+/**
+ *
+ * @author hugo.suarez
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
